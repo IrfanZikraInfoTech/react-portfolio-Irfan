6324ed8148c2b1aa574e70c1527ba308b0492f96
IrfanZikraInfoTech <irfan@zikrainfotech.com>
2024-09-30 01:45:38 +0530

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../contacts/index";
-import KevinRushProfile from "../assets/KevinRushProfile.png";
+import KevinRushProfile from "../assets/kevinRushProfile.png";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
