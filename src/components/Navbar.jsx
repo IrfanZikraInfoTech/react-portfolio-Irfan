5772b5dfaf23cd36c78b1c958a9e5a4cce3de49d
IrfanZikraInfoTech <irfan@zikrainfotech.com>
2024-09-30 01:28:55 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import logo from "../assets/KevinRushLogo.png";
+import { FaSquareXTwitter } from "react-icons/fa6";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="felx flex-shrink-0 items-center">
+        <img src={logo} alt="logo" className="mx-2 w-10" />
+      </div>
+      <div className="flex m-8 items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagram />
+        <FaSquareXTwitter />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
